1ba5c440de203148600364f53062ba41bd46b6aa
Angelo <abserenuela@ccc.edu.ph>
2025-05-13 12:08:40 +0800

    asdfasdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,13 +13,14 @@ export default defineNuxtConfig({
    app: {
      cdnURL: 'https://res.cloudinary.com/veseylab/raw/upload/v1684982764/magicmouse-2.0.0.cdn.min.js',
     head: {
-      title: 'GeloWebDev', // default fallback title
+      title: 'GeloWebDev',
+       link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/gelowebdev.svg' },
+      ], // default fallback title
       htmlAttrs: {
         lang: 'en',
       },
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/gelowebdev.svg' },
-      ],
+     
       "script": [{
         src:"https://res.cloudinary.com/veseylab/raw/upload/v1684982764/magicmouse-2.0.0.cdn.min.js"
       }],
