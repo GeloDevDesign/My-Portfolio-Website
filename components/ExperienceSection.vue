621261154a14dfaae54ad2856a738db2834293af
Angelo <abserenuela@ccc.edu.ph>
2025-05-17 17:08:18 +0800

    asdfasdf

--- components/ExperienceSection.vue
+++ components/ExperienceSection.vue
@@ -1,74 +1,84 @@
 <script setup>
+import { Building2 } from "lucide-vue-next";
 // Create an array of experience objects
 const experiences = [
   {
-    title: "Web Developer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus laborum rem tempore expedita, quis dolorum",
-    year: "2025",
-    tech_stack: [
-      { name: "Vue.js" },
-      { name: "Tailwind CSS" },
-      { name: "Node.js" }
-    ]
+    title: "Web Developer Intern",
+    description:
+      "City College of Calamba",
+    year: "2024-2025",
+    tech_stack: [{ name: "2024-2025" }],
   },
   {
-    title: "Frontend Engineer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus laborum rem tempore expedita, quis dolorum",
+    title: "Freelance Fullstack Developer",
+    description:
+      "No Company name freelance work",
     year: "2024",
-    tech_stack: [
-      { name: "React" },
-      { name: "TypeScript" },
-      { name: "SCSS" }
-    ]
+    tech_stack: [{ name: "2024-2025" }],
   },
   {
-    title: "UI/UX Designer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus laborum rem tempore expedita, quis dolorum",
+    title: "Freelance Website Designer",
+    description:
+      "No Company name freelance work",
     year: "2023",
-    tech_stack: [
-      { name: "Figma" },
-      { name: "Adobe XD" },
-      { name: "Sketch" }
-    ]
+    tech_stack: [{ name: "2024" }],
   },
-   {
-    title: "UI/UX Designer",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus laborum rem tempore expedita, quis dolorum",
+  {
+    title: "Hello World",
+    description: "Wrote my first line of code",
     year: "2023",
-    tech_stack: [
-      { name: "Figma" },
-      { name: "Adobe XD" },
-      { name: "Sketch" }
-    ]
-  }
+    tech_stack: [{ name: "2023" }, { name: "Visual Studio Code" }],
+  },
+  {
+    title: "BS Information Technology",
+    description: "City College of Calamba",
+    year: "2021",
+    tech_stack: [{ name: "2021" }, { name: "Self" }],
+  },
 ];
 </script>
 
 <template>
-  <section class="w-full lg:py-32 md:py-24 lg:space-y-20 md:space-y-14 space-y-8">
-    <SectionHeading sectionName="EXPERIENCES" />
+  <section class="w-full lg:py-24 md:py-20 md:space-y-14 space-y-8">
+    <div class="space-y-2">
+      <SectionHeading sectionName="WORK & WINS" />
+      <p class="text-gray-400 text-medium lg:max-w-2/3 md:w-full w-full lg:text-base md:text-base text-sm">
+        The highlights—where skill, hustle, and results collide.
+      </p>
+    </div>
     <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-      <div 
-        v-for="(experience, index) in experiences" 
-        :key="index" 
-        class="w-full bg-[#1E1E1E] p-6 space-y-4 rounded-lg magic-hover magic-hover__square"
+      <div
+        v-for="(experience, index) in experiences"
+        :key="index"
+        class="w-full bg-[#1E1E1E] lg:p-6 md:p-6 p-4 space-y-4 rounded-lg magic-hover magic-hover__square"
       >
-        <div class="flex justify-between items-center">
-          <h5 class="text-lg">{{ experience.title }}</h5>
-          <span class="text-sm">{{ experience.year }}</span>
-        </div>
-        <p class="text-gray-400 text-medium lg:text-sm md:text-sm text-xs">
-          {{ experience.description }}
-        </p>
-        <div class="space-x-2 flex flex-wrap gap-2">
-         
-          <!-- Tech stack badges -->
-          <div 
-            v-for="(tech, techIndex) in experience.tech_stack"
-            :key="techIndex" 
-            class="badge badge-neutral badge-sm badge-outline border-gray-300 text-gray-300 magic-hover magic-hover__square"
-          >
-            {{ tech.name }}
+        <div
+          class="flex flex-col justify-between h-full lg:gap-10 md:gap-8 gap-6"
+        >
+          <div class="flex flex-col gap-4">
+            <h5 class="lg:text-lg md:text-lg text-base font-semibold">
+              {{ experience.title }}
+            </h5>
+            <div class="flex gap-2 items-start h-full p-0">
+              <div>
+                  <Building2 size="16"/>
+              </div>
+              <p
+                class="text-gray-400 text-medium lg:text-sm md:text-sm text-xs"
+              >
+                {{ experience.description }}
+              </p>
+            </div>
+          </div>
+          <div class="flex flex-wrap gap-2 items-center h-full">
+            <!-- Tech stack badges -->
+            <div
+              v-for="(tech, techIndex) in experience.tech_stack"
+              :key="techIndex"
+              class="badge badge-neutral lg:badge-sm md:badge-sm badge-xs badge-outline border-gray-300 text-gray-300 magic-hover magic-hover__square"
+            >
+              {{ tech.name }}
+            </div>
           </div>
         </div>
       </div>
